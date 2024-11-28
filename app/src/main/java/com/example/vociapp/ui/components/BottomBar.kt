00ba2383164f845a2ce662573e02da5010de848a
PescaUnimib <g.pesce7@campus.unimib.it>
2024-11-28 18:44:15 +0100

    Stile della navbar

--- app/src/main/java/com/example/vociapp/ui/components/BottomBar.kt
+++ app/src/main/java/com/example/vociapp/ui/components/BottomBar.kt
@@ -1,15 +1,29 @@
 package com.example.vociapp.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.vociapp.ui.navigation.Screens
 import com.example.vociapp.ui.navigation.currentRoute
+import com.example.vociapp.ui.theme.ColorPalette
 
 @Composable
 fun BottomBar(navController: NavHostController) {
@@ -21,11 +35,34 @@ fun BottomBar(navController: NavHostController) {
     )
     val currentRoute = currentRoute(navController)
 
-    NavigationBar {
+    NavigationBar(
+        containerColor = ColorPalette.BackgroundNavBarColor,
+        modifier = Modifier.padding(vertical = 5.dp)
+    ) {
         items.forEach { screen ->
             NavigationBarItem(
-                icon = { Icon(screen.icon, contentDescription = screen.title) },
-                label = { Text(screen.title) },
+                icon = {
+                    Box(
+                        modifier = Modifier
+                            .offset(y = 5.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(
+                                color =
+                                if (currentRoute == screen.route)
+                                    ColorPalette.SelectedIconColor
+                                else
+                                    ColorPalette.BackgroundIconColor
+                            )
+                            .padding(10.dp)
+                    ){
+                        Icon(screen.icon, contentDescription = screen.title)
+                    }
+                     },
+                label = {
+                    Text(screen.title,
+                        style = TextStyle(fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium)
+                    )},
                 selected = currentRoute == screen.route,
                 onClick = {
                     navController.navigate(screen.route) {
@@ -43,9 +80,11 @@ fun BottomBar(navController: NavHostController) {
                     }
                 },
                 colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = MaterialTheme.colorScheme.secondary,
-                    selectedTextColor = MaterialTheme.colorScheme.secondary,
-                    indicatorColor = MaterialTheme.colorScheme.primary,
+                    unselectedTextColor = ColorPalette.IconColor,
+                    selectedTextColor = ColorPalette.IconColor,
+                    selectedIconColor = ColorPalette.BackgroundIconColor,
+                    unselectedIconColor = ColorPalette.IconColor,
+                    indicatorColor = Color.Transparent
                 )
             )
         }
