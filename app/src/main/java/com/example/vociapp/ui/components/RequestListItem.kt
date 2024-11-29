3a863b96c4ef1972fa45d83e5cedaa496c2f296a
Samuele Poretti <s.poretti@campus.unimib.it>
2024-11-29 12:17:26 +0100

    Fixed overflow with ellipsis in the items and chips

--- app/src/main/java/com/example/vociapp/ui/components/RequestListItem.kt
+++ app/src/main/java/com/example/vociapp/ui/components/RequestListItem.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,6 +33,7 @@ import com.example.vociapp.data.types.Request
 import com.example.vociapp.data.types.RequestStatus
 import com.example.vociapp.data.util.DateTimeFormatter
 import com.example.vociapp.data.util.DateTimeFormatterImpl
+import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
 fun RequestListItem(request: Request, navController: NavHostController, onClick: () -> Unit) {
@@ -80,12 +82,22 @@ fun RequestListItem(request: Request, navController: NavHostController, onClick:
                         Column {
                             Text(
                                 text = request.title,
-                                style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                                style = MaterialTheme.typography.bodyMedium.copy(
+                                    fontWeight = FontWeight.Bold,
+                                    color = MaterialTheme.colorScheme.primary
+                                ),
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                modifier = Modifier.widthIn(max = 180.dp)
                             )
                             Text(
                                 text = request.description,
-                                style = MaterialTheme.typography.bodySmall
+                                style = MaterialTheme.typography.bodySmall,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                modifier = Modifier.widthIn(max = 250.dp)
                             )
                         }
                     }
