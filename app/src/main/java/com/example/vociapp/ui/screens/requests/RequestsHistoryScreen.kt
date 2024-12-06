08cfd2aed6e8e5c164703bfbb0dcb9f3c079d0c8
Samuele Poretti <s.poretti@campus.unimib.it>
2024-12-06 20:27:21 +0100

    VAI E SCOPRI

--- app/src/main/java/com/example/vociapp/ui/screens/requests/RequestsHistoryScreen.kt
+++ app/src/main/java/com/example/vociapp/ui/screens/requests/RequestsHistoryScreen.kt
@@ -5,26 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxState
-import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -34,9 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
@@ -77,7 +58,6 @@ fun RequestsHistoryScreen(
         )
     }
 
-
     LaunchedEffect(Unit) {
         requestViewModel.getRequests()
     }
@@ -109,8 +89,6 @@ fun RequestsHistoryScreen(
 
             }
 
-
-
             RequestList(
                 requests = requests,
                 filterOption = RequestStatus.DONE,
@@ -123,100 +101,7 @@ fun RequestsHistoryScreen(
                 requestViewModel = requestViewModel,
                 homeLessViewModel = homelessViewModel
             )
-            Box(modifier = Modifier.fillMaxWidth()) {
 
-                when (requests) {
-                    is Resource.Loading -> {
-                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                    }
-
-                    is Resource.Success -> {
-                        if (doneRequests.isEmpty()) {
-                            Text(
-                                "Non ci sono richieste attive",
-                                modifier = Modifier.align(Alignment.Center)
-                            )
-                        } else {
-                            doneRequests = doneRequests.sortedWith(selectedSortOption.comparator)
-                            LazyVerticalGrid(
-                                columns = GridCells.Fixed(1),
-                                modifier = Modifier.fillMaxSize()
-                            ) {
-                                items(doneRequests) { request ->
-                                    // SwipeToDismissBoxState with necessary configuration
-                                    val density = LocalDensity.current
-                                    val swipeState = remember(density) {
-                                        SwipeToDismissBoxState(
-                                            initialValue = SwipeToDismissBoxValue.Settled, // Initial state
-                                            density = density, // Provides density for thresholds
-                                            confirmValueChange = { newValue ->
-                                                // Confirm dismissal only for the Dismissed state
-                                                newValue == SwipeToDismissBoxValue.StartToEnd
-                                            },
-                                            positionalThreshold = { totalDistance ->
-                                                // Set a threshold to trigger dismissal (e.g., 50% of the distance)
-                                                totalDistance * 0.2f
-                                            }
-                                        )
-                                    }
-
-                                    // Observe dismissal state
-                                    LaunchedEffect(swipeState.currentValue) {
-                                        if (swipeState.currentValue == SwipeToDismissBoxValue.StartToEnd) {
-                                            requestViewModel.deleteRequest(request)
-                                        }
-                                    }
-
-                                    // SwipeToDismissBox with swipe gestures
-                                    SwipeToDismissBox(
-                                        state = swipeState,
-
-                                        backgroundContent = {
-                                            // Optional background content while swiping (e.g., delete icon)
-                                            Row(
-                                                modifier = Modifier
-                                                    .fillMaxSize()
-                                                    .clip(shape = RoundedCornerShape(16.dp))
-                                                    .background(MaterialTheme.colorScheme.error),
-                                                verticalAlignment = Alignment.CenterVertically
-                                            ) {
-                                                Spacer(modifier = Modifier.width(16.dp))
-                                                Icon(
-                                                    imageVector = Icons.Default.Delete,
-                                                    contentDescription = "Delete",
-                                                    tint = Color.White
-                                                )
-                                            }
-                                        },
-                                        modifier = Modifier.fillMaxWidth(),
-                                        enableDismissFromStartToEnd = true, // Allow swipe from start to end
-                                        enableDismissFromEndToStart = false, // Allow swipe from end to start
-                                        gesturesEnabled = true // Enable gestures
-                                    ) {
-                                        // Main content inside the swipe box
-                                        RequestListItem(
-                                            request = request,
-                                            navController,
-                                            requestViewModel
-                                        ) {
-                                            showDialog = true
-                                            selectedRequest = request
-                                        }
-                                    }
-                                    Spacer(modifier = Modifier.height(8.dp))
-                                }
-                            }
-                        }
-                    }
-
-                    is Resource.Error -> {
-                        Text(
-                            text = "Error: ${requests.message}",
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                }
-            }
         }
 
         if (showDialog) {
--- app/src/main/java/com/example/vociapp/ui/screens/requests/RequestsScreen.kt
+++ app/src/main/java/com/example/vociapp/ui/screens/requests/RequestsScreen.kt
@@ -1,30 +1,18 @@
 package com.example.vociapp.ui.screens.requests
 
-import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Checklist
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.History
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
@@ -35,10 +23,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxState
-import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -51,7 +35,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
@@ -165,108 +148,6 @@ fun RequestsScreen(
                     requestViewModel = requestViewModel,
                     homeLessViewModel = homelessViewModel
                 )
-
-
-                Box(modifier = Modifier.fillMaxWidth()) {
-
-                    when (requests) {
-                        is Resource.Loading -> {
-                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                        }
-
-                        is Resource.Success -> {
-                            if (todoRequests.isEmpty()) {
-                                Text(
-                                    "Non ci sono richieste attive",
-                                    modifier = Modifier.align(Alignment.Center)
-                                )
-                            } else {
-                                todoRequests =
-                                    todoRequests.sortedWith(selectedSortOption.comparator)
-                                LazyVerticalGrid(
-                                    columns = GridCells.Fixed(1),
-                                    modifier = Modifier.fillMaxSize()
-                                ) {
-                                    items(todoRequests) { request ->
-                                        // SwipeToDismissBoxState with necessary configuration
-                                        val density = LocalDensity.current
-                                        val swipeState = remember(density) {
-                                            SwipeToDismissBoxState(
-                                                initialValue = SwipeToDismissBoxValue.Settled, // Initial state
-                                                density = density, // Provides density for thresholds
-                                                confirmValueChange = { newValue ->
-                                                    // Confirm dismissal only for the Dismissed state
-                                                    newValue == SwipeToDismissBoxValue.StartToEnd
-                                                },
-                                                positionalThreshold = { totalDistance ->
-                                                    // Set a threshold to trigger dismissal (e.g., 50% of the distance)
-                                                    totalDistance * 0.2f
-                                                }
-                                            )
-                                        }
-
-                                        // Observe dismissal state
-                                        LaunchedEffect(swipeState.currentValue) {
-                                            if (swipeState.currentValue == SwipeToDismissBoxValue.StartToEnd) {
-                                                requestViewModel.requestDone(request)
-                                            }
-                                        }
-
-                                        // SwipeToDismissBox with swipe gestures
-                                        SwipeToDismissBox(
-                                            state = swipeState,
-
-                                            backgroundContent = {
-                                                // Optional background content while swiping (e.g., delete icon)
-                                                Row(
-                                                    modifier = Modifier
-                                                        .fillMaxSize()
-                                                        .clip(shape = RoundedCornerShape(16.dp))
-                                                        .background(MaterialTheme.colorScheme.primary),
-                                                    verticalAlignment = Alignment.CenterVertically
-                                                ) {
-                                                    Spacer(modifier = Modifier.width(16.dp))
-                                                    Icon(
-                                                        imageVector = Icons.Default.Check,
-                                                        contentDescription = "Delete",
-                                                        tint = Color.White
-                                                    )
-                                                }
-                                            },
-                                            modifier = Modifier.fillMaxWidth(),
-                                            enableDismissFromStartToEnd = true, // Allow swipe from start to end
-                                            enableDismissFromEndToStart = false, // Allow swipe from end to start
-                                            gesturesEnabled = true // Enable gestures
-                                        ) {
-                                            // Main content inside the swipe box
-                                            RequestListItem(
-                                                request = request,
-                                                navController,
-                                                requestViewModel
-                                            ) {
-                                                showDialog = true
-                                                selectedRequest = request
-                                            }
-                                        }
-                                        Spacer(modifier = Modifier.height(8.dp))
-                                    }
-
-                                }
-
-                            }
-                        }
-
-                        is Resource.Error -> {
-                            Text(
-                                text = "Error: ${requests.message}",
-                                modifier = Modifier.align(Alignment.Center)
-                            )
-                        }
-
-                    }
-                }
-
-
             }
 
             FloatingActionButton(
