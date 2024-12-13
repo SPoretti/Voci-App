7f9970b1592cbf49eda98b57b7d0982e563e81e7
EnricoZhou <zhouenrico@gmail.com>
2024-12-13 19:14:40 +0100

    provato a fare la modifica dell email senza successo

--- app/src/main/java/com/example/vociapp/ui/screens/profiles/userProfile/UpdateUserProfileScreen.kt
+++ app/src/main/java/com/example/vociapp/ui/screens/profiles/userProfile/UpdateUserProfileScreen.kt
@@ -1,5 +1,8 @@
 package com.example.vociapp.ui.screens.profiles.userProfile
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.widget.EditText
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,8 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,24 +37,62 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.vociapp.R
+//import androidx.preference.isNotEmpty
 import com.example.vociapp.di.LocalServiceLocator
 import com.example.vociapp.ui.viewmodels.AuthResult
+import com.example.vociapp.ui.viewmodels.VolunteerViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.example.vociapp.data.repository.VolunteerRepository
+import com.example.vociapp.data.types.Volunteer
+import com.example.vociapp.data.remote.FirestoreDataSource
+import com.example.vociapp.data.util.Resource
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import kotlinx.coroutines.tasks.await
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import kotlinx.coroutines.tasks.await
+
 
 @Composable
 fun UpdateUserProfileScreen(
     navController: NavHostController
 ) {
+    val context = LocalContext.current
     val serviceLocator = LocalServiceLocator.current
     val authViewModel = serviceLocator.getAuthViewModel()
     val currentProfile = authViewModel.getCurrentUserProfile()
     var displayName by remember { mutableStateOf(currentProfile?.displayName ?: "") }
+    // val surname by remember { mutableStateOf(currentProfile?.surname ?: "") }
+    var email by remember { mutableStateOf(currentProfile?.email ?: "") }
+    var phoneNumber by remember { mutableStateOf(currentProfile?.phoneNumber ?: "") }
     var photoUrl by remember { mutableStateOf(currentProfile?.photoUrl ?: "") }
     var showError by remember { mutableStateOf(false) }
     var errorMessage by remember { mutableStateOf("") }
     var isUpdating by remember { mutableStateOf(false) }
     var isNavigatingBack by remember { mutableStateOf(false) }
+
+    var password by remember { mutableStateOf("") } // Add a state for password input
+
+    var currentPassword by remember { mutableStateOf("") }
+    var newEmail by remember { mutableStateOf("") }
+
 
     /*LaunchedEffect(key1 = "updateUserProfile") {
         isNavigatingBack = false
@@ -109,8 +153,49 @@ fun UpdateUserProfileScreen(
                     ProfileTextField(
                         value = displayName,
                         onValueChange = { displayName = it },
-                        label = "Username",
+                        label = "Name",
                         icon = Icons.Default.Person
+                    )
+
+//                    ProfileTextField(
+//                        value = surname,
+//                        onValueChange = { surname = it },
+//                        label = "Name",
+//                        icon = Icons.Default.Person
+//                    )
+
+                    ProfileTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = "Email",
+                        icon = Icons.Default.Email
+                    )
+
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Lock,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        visualTransformation = PasswordVisualTransformation(),
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(8.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = MaterialTheme.colorScheme.primary,
+                            unfocusedBorderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                        )
+                    )
+
+                    ProfileTextField(
+                        value = phoneNumber,
+                        onValueChange = { phoneNumber = it },
+                        label = "Phone Number",
+                        icon = Icons.Default.Phone
                     )
 
                     ProfileTextField(
@@ -120,8 +205,50 @@ fun UpdateUserProfileScreen(
                         icon = Icons.Default.Face
                     )
 
+                    OutlinedTextField(
+                        value = newEmail,
+                        onValueChange = { newEmail = it },
+                        label = { Text("New Email") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Email,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(8.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = MaterialTheme.colorScheme.primary,
+                            unfocusedBorderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                        )
+                    )
+
+                    // Add a field for password input
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Lock,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary
+                            )
+                        },
+                        visualTransformation = PasswordVisualTransformation(),
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(8.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = MaterialTheme.colorScheme.primary,
+                            unfocusedBorderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+                        )
+                    )
+
                     Button(
-                        onClick = { isUpdating = true },
+                        onClick = {
+                            isUpdating = true
+                        },
                         enabled = !isUpdating,
                         modifier = Modifier.fillMaxWidth(),
                         shape = RoundedCornerShape(8.dp)
@@ -147,17 +274,90 @@ fun UpdateUserProfileScreen(
 
     LaunchedEffect(isUpdating) {
         if (isUpdating) {
-            val result = authViewModel.updateUserProfile(displayName, photoUrl)
-            if (result is AuthResult.Failure) {
-                showError = true
-                errorMessage = result.message
-            } else {
-                navController.popBackStack()
+            // funziona trova "id" del profilo utente
+            val currentId = serviceLocator.getVolunteerViewModel().getVolunteerId().toString()
+//            println("this is the document id: " + currentId)
+            // Usage
+            val db = FirebaseFirestore.getInstance()
+            // TODO: modifica-> in modo che trova direttamente l utente
+            val docRef = db.collection("volunteers").document("bptEAkUWtN879UNFni84")
+
+            val auth = FirebaseAuth.getInstance()
+            val currentUser = auth.currentUser
+
+            val updates = mapOf(
+                "name" to displayName,
+                // "email" to email,
+                "phone_number" to phoneNumber,
+                "photoUrl" to photoUrl
+            ).filterValues { it.isNotEmpty() }
+
+            // attiva la funzione updateVolunteer che dovrebbe modificare solo i campi inseriti ma modifica anche quelli già presenti nel database (-> null)
+//            val result = serviceLocator.getVolunteerViewModel().updateVolunteer(updates)
+
+            if (updates.isNotEmpty()) {
+                try {
+                    val querySnapshot = db.collection("volunteers")
+                        .whereEqualTo("id", currentId) // Query by "id" field
+                        .get()
+                        .await()
+
+                    if (querySnapshot.documents.isNotEmpty()) {
+                        val documentId = querySnapshot.documents[0].id // Get the document ID
+                        db.collection("volunteers")
+                            .document(documentId) // Use the document ID for update
+                            .set(updates, SetOptions.merge())
+                            .await()
+                        Resource.Success(Unit)
+                    } else {
+                        Resource.Error("Volunteer not found") // Handle case where request is not found
+                    }
+                } catch (e: Exception) {
+                    Resource.Error(e.message ?: "An unknown error occurred")
+                }
             }
-            isUpdating = false
+
+            currentUser?.let {
+                it.verifyBeforeUpdateEmail(newEmail).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(
+                            "UpdateEmail",
+                            "User email address updated."
+                        )
+                        Toast.makeText (context, "Verification email sent to update address.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        task.exception?.let { exception ->
+                            Log.e(
+                                "UpdateEmail",
+                                "Failed to send verification email: ${exception.message}"
+                            )
+                            Toast.makeText (context, "Failed to send verification email.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            } ?: run {
+                Toast.makeText(
+                    context,
+                    "No user is currently signed in.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            val user = Firebase.auth.currentUser
+
+            user!!.verifyBeforeUpdateEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "User email address updated.")
+                    }
+                }
+
         }
+        isUpdating = false
     }
 }
+
+
 
 @Composable
 fun ProfileTextField(
@@ -184,4 +384,19 @@ fun ProfileTextField(
             unfocusedBorderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
         )
     )
+}
+
+@Composable
+fun CollectProfileUpdates(
+    displayName: String,
+    email: String,
+    phoneNumber: String,
+    photoUrl: String
+): Map<String, Any> {
+    return mapOf(
+        "displayName" to displayName,
+        "email" to email,
+        "phoneNumber" to phoneNumber,
+        "photoUrl" to photoUrl
+    ).filterValues { it.isNotEmpty() }
 }
