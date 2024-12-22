7f8a891fbfe24a2cefa54e980dfd5b27fd6d7de0
PescaUnimib <g.pesce7@campus.unimib.it>
2024-12-22 10:36:17 +0100

    Modifica alle barreee

--- app/src/main/java/com/example/vociapp/ui/components/CustomSnackBar.kt
+++ app/src/main/java/com/example/vociapp/ui/components/CustomSnackBar.kt
@@ -1,14 +1,12 @@
 package com.example.vociapp.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.SnackbarData
