a4af98aaa4b3f4cbb4a4d67df94fb0ea81be6470
PescaUnimib <130898089+PescaUnimib@users.noreply.github.com>
2025-01-04 23:25:17 +0100

    Inserito campo photoUrl nel volontario
    Adesso si può vedere l'immagine deglia altri volontari nel profilo

--- app/src/main/java/com/example/vociapp/ui/screens/profiles/volunteer/ProfileVolunteerScreen.kt
+++ app/src/main/java/com/example/vociapp/ui/screens/profiles/volunteer/ProfileVolunteerScreen.kt
@@ -1,5 +1,6 @@
 package com.example.vociapp.ui.screens.profiles.volunteer
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,21 +25,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.example.vociapp.data.local.database.Volunteer
 import com.example.vociapp.data.util.Resource
 import com.example.vociapp.di.LocalServiceLocator
 import com.example.vociapp.ui.components.ProfileInfoItem
+import com.example.vociapp.ui.viewmodels.AuthViewModel
 
 @Composable
 fun ProfileVolunteerScreen(creatorId: String) {
     val serviceLocator = LocalServiceLocator.current
     val volunteerViewModel = serviceLocator.obtainVolunteerViewModel()
+    val authViewModel = serviceLocator.obtainAuthViewModel()
 
     LaunchedEffect(key1 = creatorId) {
         volunteerViewModel.getVolunteerById(creatorId)
@@ -72,20 +82,36 @@ fun ProfileVolunteerScreen(creatorId: String) {
                     is Resource.Success -> {
                         val volunteer = volunteerResource.data
 
-                        // Profile picture placeholder
-                        Box(
-                            modifier = Modifier
-                                .size(120.dp)
-                                .clip(CircleShape)
-                                .background(MaterialTheme.colorScheme.surface),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Person,
-                                contentDescription = "Profile Picture",
-                                modifier = Modifier.size(64.dp),
-                                tint = MaterialTheme.colorScheme.primary
-                            )
+                        val initials =
+                            "${volunteer?.name?.firstOrNull() ?: ""}${volunteer?.surname?.firstOrNull() ?: ""}".uppercase()
+
+                        //Profile picture
+                        if (volunteer != null) {
+                            if (volunteer.photoUrl.isNotEmpty()) {
+                                AsyncImage(
+                                    model = volunteer.photoUrl,
+                                    contentDescription = "Profile Picture",
+                                    modifier = Modifier
+                                        .size(120.dp)
+                                        .clip(CircleShape),
+                                    contentScale = ContentScale.Crop
+                                )
+                            } else {
+                                Box(
+                                    contentAlignment = Alignment.Center,
+                                    modifier = Modifier
+                                        .size(130.dp)
+                                        .clip(CircleShape)
+                                        .background(MaterialTheme.colorScheme.primary)
+                                ) {
+                                    Text(
+                                        text = initials,
+                                        style = MaterialTheme.typography.headlineMedium,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 60.sp
+                                    )
+                                }
+                            }
                         }
 
                         Text(
